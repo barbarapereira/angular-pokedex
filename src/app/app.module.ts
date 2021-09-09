c73a3bf2aed6af8d53dc1bcaef1101e806ef5913
Barbara Pereira <barbara.pereira1990@gmail.com>
2021-09-09 09:45:45 -0300

    integracao com a api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,17 @@ import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonService } from './services/pokemon.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     PokemonListComponent,
     PokemonCardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [PokemonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
